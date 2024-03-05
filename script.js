2189db8d2ab946265ceababce44c2faa41ba31cc
rvngrd <rrvngrdd@gmail.com>
2024-03-05 19:42:54 +0330

    declaring a variable(camelcase in variables)

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("hello mashti :D")
+// alert("hello mashti :D")
+
+// declaring variables
+let a = "aha";
+//camelCase is a variable naming convention where the first word is always in lowercase and every other word starts with a capital letter
+let firstName = "Iman";
+console.log(firstName);
