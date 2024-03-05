bd7d45651f83ba8b447596656710e8335971a310
rvngrd <rrvngrdd@gmail.com>
2024-03-05 20:01:26 +0330

    let and var

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ let a = "aha";
 //camelCase is a variable naming convention where the first word is always in lowercase and every other word starts with a capital letter
 let firstName = "Iman";
 console.log(firstName);
+
+// Variables declared with the var always have Global Scope
+// let has a scope and the variable can't be used out of its scope(Function Scope)
+
+{
+    let b = "bb";
+    var x = "xx";
+    x = 12;
+}
+console.log(x);
+console.log(b);     //Uncaught ReferenceError: b is not defined
+
+// Variables defined with let can not be redeclared
+// Variables defined with var can be redeclared
+var y = "John Doe";
+var y = 0;
