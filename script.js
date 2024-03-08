00e49b14b8a5917a8805500eff069058992d2c93
rvngrd <rrvngrdd@gmail.com>
2024-03-08 12:52:56 +0330

    JavaScript Operators

--- script.js
+++ script.js
@@ -69,3 +69,42 @@ var carName;
     let m;
     console.log(typeof m); //undefined
 }
+
+// JavaScript Operators
+{
+    let a = 3;
+    a--;
+    let x = (100 + 50) * a;
+}
+
+// Exponential Notation
+{
+    let y = 123e5;    // 12300000
+    let z = 123e-5;   // 0.00123
+}
+
+// JavaScript Comparison Operators
+/*
+==	equal to
+===	equal value and equal type
+!=	not equal
+!==	not equal value or not equal type
+*/
+
+// When used on strings, the + operator is called the concatenation operator
+{
+    let text1 = "What a very ";
+    text1 += "nice day";
+}
+
+// JavaScript Logical Operators
+/*
+&&	logical and
+||	logical or
+!	logical not
+*/ 
+{
+    let x = true;
+    let y = false;
+    console.log(x && y);
+}
